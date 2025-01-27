acc67da312e3647de6d4743108510d15d9a1a804
beatriz_g <beatrizgue20@gmail.com>
2025-01-27 14:27:45 -0800

    Added notes to js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
+// Variables to select the fans, fish, and pets elements in footer
 var fans = document.querySelector(".fans");
 var fish = document.querySelector(".fish");
 var pets = document.querySelector(".pets");
+
 var themeSwitchInput = document.querySelector("#toggle");
 var ball = document.querySelector(".ball");
 var h1 = document.querySelector("h1");
 var body = document.querySelector("body");
 
+// Mouseover events for fans, fish, and pets to change the text of elements
 fans.addEventListener("mouseover", function () {
     fans.innerText = "123k";
 });
@@ -18,6 +21,7 @@ pets.addEventListener("mouseover", function() {
     pets.innerText = "20k";
 });
 
+// Event listener that listens for a click event on the Dark/Light toggle input
 themeSwitchInput.addEventListener("click", function() {
     if (body.classList.contains("light")) {
         body.classList.remove("light");
